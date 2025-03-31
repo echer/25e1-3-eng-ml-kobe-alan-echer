24a91ba27374170a8200608434bd5dfc8feac5df
Alan Echer <echer>
2025-03-31 18:04:20 -0300

    adicionado node e pipeline para drop column split test e drop na

--- src/25e1_3_eng_ml_kobe_alan_echer/pipelines/acertos_kobe/nodes.py
+++ src/25e1_3_eng_ml_kobe_alan_echer/pipelines/acertos_kobe/nodes.py
@@ -2,3 +2,24 @@
 This is a boilerplate pipeline 'acertos_kobe'
 generated using Kedro 0.19.12
 """
+
+from sklearn.model_selection import train_test_split
+from kedro.config import OmegaConfigLoader 
+
+parameters = OmegaConfigLoader(conf_source=".")['parameters']
+
+def kobe_intermediate_drop_columns(dataset):
+    dataset = dataset.drop(columns=['action_type','combined_shot_type','game_event_id','game_id','loc_x','loc_y','season','seconds_remaining','shot_type','shot_zone_area','shot_zone_basic','shot_zone_range','team_id','team_name','game_date','matchup','opponent','shot_id'])
+    return dataset
+
+def kobe_raw_parquet_split_train_test(dataset):
+    train, test = train_test_split(
+        dataset, 
+        test_size=parameters['train_test_split_ratio'], 
+        stratify=dataset[parameters['y_column']]
+    ) 
+    return train, test
+
+def kobe_intermediate_shot_made_flag_drop_na(dataset):
+    #dataset.dropna()
+    pass
--- src/25e1_3_eng_ml_kobe_alan_echer/pipelines/acertos_kobe/pipeline.py
+++ src/25e1_3_eng_ml_kobe_alan_echer/pipelines/acertos_kobe/pipeline.py
@@ -4,7 +4,33 @@ generated using Kedro 0.19.12
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from . import nodes
+from kedro.config import OmegaConfigLoader 
 
+parameters = OmegaConfigLoader(conf_source=".")['parameters']
+
+def isProduction():
+    return parameters['is_production']
+
+def getRawDataset():
+    return 'kobe_raw_dev@parquet' if(not isProduction()) else 'kobe_raw_prod@parquet'
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+            nodes.kobe_intermediate_drop_columns,
+            inputs=[getRawDataset()],
+            outputs='kobe_intermediate_drop_columns'
+        ),
+        node(
+            nodes.kobe_intermediate_shot_made_flag_drop_na,
+            inputs=['kobe_intermediate_drop_columns'],
+            outputs='kobe_intermediate_shot_made_flag_drop_na',
+        ),
+        node(
+            nodes.kobe_raw_parquet_split_train_test,
+            inputs=['kobe_intermediate_shot_made_flag_drop_na'],
+            outputs=['base_train','base_test'],
+        ),
+        
+    ])
