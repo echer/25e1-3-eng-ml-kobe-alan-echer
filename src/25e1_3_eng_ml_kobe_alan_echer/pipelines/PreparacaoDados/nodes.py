b2abbb65ddfd8529819993b89e9d5bc8a76ab61a
Alan Echer <echer>
2025-04-01 19:29:30 -0300

    adicionado geracao de dois modelos

--- src/25e1_3_eng_ml_kobe_alan_echer/pipelines/PreparacaoDados/nodes.py
+++ src/25e1_3_eng_ml_kobe_alan_echer/pipelines/PreparacaoDados/nodes.py
@@ -5,6 +5,8 @@ generated using Kedro 0.19.12
 from sklearn.model_selection import train_test_split
 from kedro.config import OmegaConfigLoader 
 from pycaret.classification import *
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 parameters = OmegaConfigLoader(conf_source=".")['parameters']
 
@@ -32,5 +34,14 @@ def train_model_pycarret(dataset):
         log_experiment='mlflow', 
         experiment_name=parameters['mflow_experiment_name'], 
         session_id=parameters['session_id'])
-    best = experiment.compare_models(n_select=2)
-    return best[0], best[1]
+    best = experiment.compare_models(n_select=15)
+    model_1 = None
+    model_2 = None
+
+    for model in best:
+        if(isinstance(model, AdaBoostClassifier)):
+            model_1 = model
+        if(isinstance(model, DecisionTreeClassifier)):
+            model_2 = model
+
+    return model_1, model_2
